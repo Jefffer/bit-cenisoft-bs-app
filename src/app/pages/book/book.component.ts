2a927cf173216913774bc2254e05349658f117b7
Jefferson Rodriguez <jefferson.rodriguez@exsis.com.co>
2021-02-21 21:38:37 -0500

    Book component changes.

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
+
 })
 export class BookComponent implements OnInit {
   bookForm: FormGroup;
+  amount = 1;
+  unitValue = 80000;
+  total = this.unitValue ;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {    
     console.log('test')
+    // console.log(this.amount);
     this.bookForm = this.formBuilder.group({
       amount: ['', ]
     });
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    // console.log(this.amount);
+
+    // console.log(changes['amount'].currentValue);
+  }
+
   updateTotal() {
   }
 
-  unitValue = 80000;
-  total = this.unitValue;
+  
 
 }
